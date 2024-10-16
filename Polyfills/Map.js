c4f7afa67651110e7f1aeb5714524889485d5bfe
Piyush023 <104673448+Piyush023@users.noreply.github.com>
2024-10-16 15:09:21 +0530

    Priettier In the File

--- Polyfills/Map.js
+++ Polyfills/Map.js
@@ -1,18 +1,18 @@
-const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5];
 
-// Map Polyfill - 
+// Map Polyfill -
 
 // Inbuilt Map -
-const res = arr.map((item) => item * 2)
-console.log(res)
+const res = arr.map((item) => item * 2);
+console.log(res);
 
-// Custom Implementation of Map Function - 
+// Custom Implementation of Map Function -
 Array.prototype.myMap = function (cb) {
-    let arr = []
-    for(let i=0;i<this.length;i++){
-        arr.push(cb(this[i]))
-    }
-    return arr;
-}
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    arr.push(cb(this[i]));
+  }
+  return arr;
+};
 
-console.log(arr.myMap((item) => item*2))
+console.log(arr.myMap((item) => item * 2));
