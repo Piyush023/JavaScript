54e6d67a9a043212fbe0ec9acba564653b148dc0
Piyush023 <104673448+Piyush023@users.noreply.github.com>
2025-03-21 12:18:15 +0530

    Linting Issues Fixed.

--- Chai_JS/09_Advance/01_Promises.js
+++ Chai_JS/09_Advance/01_Promises.js
@@ -1,37 +1,48 @@
-// Promises - 
+// Promises -
 const p1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log('This is a Promise Doing some Async Task')
-        resolve()
-    }, 2000)
-})
+  setTimeout(() => {
+    console.log('This is a Promise Doing some Async Task');
+    resolve();
+  }, 2000);
+});
 
 p1.then(() => {
-    console.log("This is the Then of the Promise P1")
-})
+  console.log('This is the Then of the Promise P1');
+});
 
-
-// Promises - 
+// Promises -
 const p2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const error = true;
-        if (error) {
-            reject({ message: 'Error in this Call' })
-        }
-        console.log('This is a Promise Doing some Async Task and passing value to the .then')
-        resolve({ message: 'Hello World' })
-    }, 2000)
-})
+  setTimeout(() => {
+    const error = true;
+    if (error) {
+      reject({ message: 'Error in this Call' });
+    }
+    console.log(
+      'This is a Promise Doing some Async Task and passing value to the .then'
+    );
+    resolve({ message: 'Hello World' });
+  }, 2000);
+});
 
 p2.then((data) => {
-    console.log(`This is the Then of the Promise P2 and the message: ${data.message}`)
-}).catch((err) => { console.log(`This is the Catch of the Promise P2 and the message: ${err.message}`) })
-
+  console.log(
+    `This is the Then of the Promise P2 and the message: ${data.message}`
+  );
+}).catch((err) => {
+  console.log(
+    `This is the Catch of the Promise P2 and the message: ${err.message}`
+  );
+});
 
 const getGitStats = fetch('https://api.github.com/users/Piyush023')
-    .then((res) => { return res.json() }).then((data)=>{
-        console.log(data)
-    })
-    .catch((e) => { console.log('e', e) })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log('e', e);
+  });
 
-getGitStats
+getGitStats;
--- Chai_JS/10_OOPS/03_Prototype.js
+++ Chai_JS/10_OOPS/03_Prototype.js
@@ -3,65 +3,63 @@
 
 // console.log(myName.trueLength);
 
-
-let myHeros = ["thor", "spiderman"]
-
+let myHeros = ['thor', 'spiderman'];
 
 let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
+  thor: 'hammer',
+  spiderman: 'sling',
 
-    getSpiderPower: function(){
-        console.log(`Spidy power is ${this.spiderman}`);
-    }
-}
+  getSpiderPower: function () {
+    console.log(`Spidy power is ${this.spiderman}`);
+  },
+};
 
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
-}
+Object.prototype.hitesh = function () {
+  console.log(`hitesh is present in all objects`);
+};
 
-Array.prototype.heyHitesh = function(){
-    console.log(`Hitesh says hello`);
-}
+Array.prototype.heyHitesh = function () {
+  console.log(`Hitesh says hello`);
+};
 
 // heroPower.hitesh()
 // myHeros.hitesh()
 // myHeros.heyHitesh()
 // heroPower.heyHitesh()
 
-// Inheritance - 
+// Inheritance -
 
 const User = {
-    name: "chai",
-    email: "chai@google.com"
-}
+  name: 'chai',
+  email: 'chai@google.com',
+};
 
 const Teacher = {
-    makeVideo: true
-}
+  makeVideo: true,
+};
 
 const TeachingSupport = {
-    isAvailable: false
-}
+  isAvailable: false,
+};
 
 const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport // Old method to Inherit the Properties of the Parent.
-}
+  makeAssignment: 'JS assignment',
+  fullTime: true,
+  __proto__: TeachingSupport, // Old method to Inherit the Properties of the Parent.
+};
 
-Teacher.__proto__ = User  // Old method to Inherit the Properties of the Parent.
+Teacher.__proto__ = User; // Old method to Inherit the Properties of the Parent.
 
-// Modern Syntax - 
-Object.setPrototypeOf(TeachingSupport, Teacher)  // Modern method to Inherit the Properties of the Parent.
+// Modern Syntax -
+Object.setPrototypeOf(TeachingSupport, Teacher); // Modern method to Inherit the Properties of the Parent.
 
-let anotherUsername = "ChaiAurCode     "
+let anotherUsername = 'ChaiAurCode     ';
 
-String.prototype.trueLength = function(){
-    console.log(`${this}`);
-    console.log(`True length is: ${this.trim().length}`);
-}
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`True length is: ${this.trim().length}`);
+};
 
-anotherUsername.trueLength()
-"hitesh".trueLength()
-"iceTea".trueLength()
+anotherUsername.trueLength();
+'hitesh'.trueLength();
+'iceTea'.trueLength();
